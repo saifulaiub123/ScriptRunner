8dfc8e409107f2af63dbb992a451a2fea57f00ad
Saiful Islam <saifulprogrammer@gmail.com>
2023-01-17 17:34:34 +0600

    initial change

---
+++ ScriptRunnerApi/UM.Infrastructure/Dependency/RepositoryResolutionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UM.Domain.IRepository;
+using UM.Infrastructure.Repository;
+
+namespace UM.Infrastructure.Dependency
+{
+    public static class RepositoryResolutionConfiguration
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IOtpRepository, OtpRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            return services;
+        }
+    }
+}
